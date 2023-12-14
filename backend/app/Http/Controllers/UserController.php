7b605d10dcba6c7f2d8deb7201a4018c2e30a32c
Ignacio Avendaño <146590243+funktasthicc@users.noreply.github.com>
2023-12-14 12:51:50 -0300

    fix: Edit user

--- backend/app/Http/Controllers/UserController.php
+++ backend/app/Http/Controllers/UserController.php
@@ -2,99 +2,47 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 use Tymon\JWTAuth\Facades\JWTAuth;
 use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
-    /**
-     * Actualiza la información del usuario.
-     *
-     * @param Request $request
-     * @return \Illuminate\Http\JsonResponse
-     */
-    public function updateProfile(Request $request)
+    public function editProfile(Request $request, $id)
     {
-        try {
-            // Obtener el usuario autenticado
-            $user = JWTAuth::user();
+        // Obtener el usuario autenticado
+        $user = JWTAuth::user();
 
-            // Verificar si el usuario está autenticado
-            if (!$user) {
-                return response()->json(['error' => 'Usuario no autenticado'], 401);
-            }
-
-            // Validación de datos para la actualización de la contraseña
-            $messages = validationMessages();
-            $this->validate($request, [
-                'full_name' => 'required|string|min:10|max:150',
-                'email' => 'required|string|regex:/^[^@]+@[^@.]+.[^@]+$/|ends_with:ucn.cl,alumnos.ucn.cl,disc.ucn.cl,ce.ucn.cl',
-                'year' => 'required|numeric|min:1900|max:' . (int)date('Y'),
-            ], $messages);
-
-            $errors = [];
-
-            if ($request->input('full_name') === $user->full_name) {
-                $errors['full_name'] = 'El nombre completo coincide con el actual.';
-            }
-
-            if ($request->input('email') === $user->email) {
-                $errors['email'] = 'El correo electrónico coincide con el actual.';
-            }
-
-            if ($request->input('year') == $user->year) {
-                $errors['year'] = 'El año coincide con el actual.';
-            }
-
-            if (!empty($errors)) {
-                return response()->json(['errors' => $errors], 422);
-            }
-
-            // Actualizar la información del usuario
-            $user->update([
-                'full_name' => $request->input('full_name'),
-                'email' => $request->input('email'),
-                'year' => $request->input('year'),
-            ]);
-
-            // Crear un conjunto de datos específico para la respuesta
-            $responseData = [
-                'success' => 'Perfil actualizado exitosamente',
-                'updated_fields' => [
-                    'full_name' => $request->input('full_name'),
-                    'email' => $request->input('email'),
-                    'year' => $request->input('year'),
-                ],
-            ];
-
-            // Respuesta JSON con información de la actualización exitosa
-            return response()->json($responseData, 200);
-
-        } catch (\Illuminate\Validation\ValidationException $e) {
-            // Error de validación
-            $errors = $e->validator->errors()->getMessages();
-            return response()->json(['errors' => $errors], 422);
-        } catch (\Exception $e) {
-            // Excepción general
-            return response()->json(['error' => $e->getMessage()], 500);
+        // Verificar si el usuario está autenticado
+        if (!$user) {
+            return response()->json(['error' => 'Usuario no autenticado'], 401);
         }
+
+        // Verificar si el usuario que solicita la edición es el mismo que el usuario a editar
+        if ($user->id != $id) {
+            return response()->json(['message' => 'No tienes permiso para editar este usuario'], 403);
+        }
+
+        // Encontrar y retornar los datos específicos del usuario
+        $userData = User::select('year', 'full_name', 'email')->find($id);
+
+        if (!$userData) {
+            return response()->json(['message' => 'Usuario no encontrado'], 404);
+        }
+
+        return response()->json(['user' => $userData]);
     }
 
+    public function updateProfile(Request $request, $id)
+    {}
 
-        /**
-     * Actualización de la contraseña del usuario.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\JsonResponse
-     */
     public function updatePassword(Request $request)
     {
         try {
             // Obtener el usuario autenticado
             $user = JWTAuth::user();
 
-            // Verificar si el usuario está autenticado
             if (!$user) {
                 return response()->json(['error' => 'Usuario no autenticado'], 401);
             }
