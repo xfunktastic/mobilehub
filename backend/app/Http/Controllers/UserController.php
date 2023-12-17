9d9896cf8193dd8061cec2979145b45801e01a06
Ignacio Avendaño <146590243+funktasthicc@users.noreply.github.com>
2023-12-17 12:05:41 -0300

    fix: Update profile

--- backend/app/Http/Controllers/UserController.php
+++ backend/app/Http/Controllers/UserController.php
@@ -35,8 +35,65 @@ class UserController extends Controller
 
     public function updateProfile(Request $request)
     {
+        try {
+            // Obtener el usuario autenticado
+            $user = JWTAuth::user();
 
+            if (!$user) {
+                return response()->json(['error' => 'Usuario no autenticado'], 401);
+            }
+
+            $error = [];
+
+            // Verificar si el nombre completo es igual
+            if ($request->input('full_name') === $user->full_name) {
+                $error['full_name'] = 'El nombre completo no ha presentado cambios.';
+            }
+
+            // Verificar si el correo electrónico es igual
+            if ($request->input('email') === $user->email) {
+                $error['email'] = 'El correo electrónico no ha presentado cambios.';
+            }
+
+            // Verificar si el año es igual (usando == para comparación)
+            if ($request->input('year') == $user->year) {
+                $error['year'] = 'El año no ha presentado cambios.';
+            }
+
+            // Si hay errores, devolver los mensajes de error
+            if (!empty($error)) {
+                return response()->json(['error' => $error], 400);
+            }
+
+            // Validación de datos para la actualización de la contraseña
+            $messages = validationMessages();
+            $this->validate($request, [
+                'full_name' => 'required|string|min:10|max:150',
+                'email' => 'required|string|regex:/^[^@]+@[^@.]+.[^@]+$/|ends_with:ucn.cl,alumnos.ucn.cl,disc.ucn.cl,ce.ucn.cl|unique:users,email',
+                'year' => 'required|numeric|min:1900|max:' . (int)date('Y'),
+            ], $messages);
+
+            // Actualizar la información del usuario
+            $user->full_name = $request->input('full_name');
+            $user->email = $request->input('email');
+            $user->year = $request->input('year');
+            $user->save();
+
+            // Respuesta JSON con información de la actualización exitosa de la contraseña
+            return response()->json([
+                'success' => 'Información actualizada exitosamente',
+            ], 200);
+
+        } catch (\Illuminate\Validation\ValidationException $e) {
+            // Error de validación
+            $errors = $e->validator->errors()->getMessages();
+            return response()->json(['errors' => $errors], 422);
+        } catch (\Exception $e) {
+            // Excepción general
+            return response()->json(['error' => $e->getMessage()], 500);
+        }
     }
+
 
     public function updatePassword(Request $request)
     {
