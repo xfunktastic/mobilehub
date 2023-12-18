ed2b632dc9a6146d68a2b29b4d445bfe5975af5a
Ignacio Avendaño <146590243+funktasthicc@users.noreply.github.com>
2023-12-17 22:31:48 -0300

    docs: Add documentation to Usercontroller

--- backend/app/Http/Controllers/UserController.php
+++ backend/app/Http/Controllers/UserController.php
@@ -6,9 +6,17 @@ use App\Models\User;
 use Illuminate\Http\Request;
 use Tymon\JWTAuth\Facades\JWTAuth;
 use Illuminate\Support\Facades\Hash;
+use Tymon\JWTAuth\Exceptions\JWTException;
+use Illuminate\Validation\ValidationException;
 
 class UserController extends Controller
 {
+    /**
+     * Obtiene y muestra la información del usuario autenticado.
+     *
+     * @param  Request  $request
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function editProfile(Request $request)
     {
         // Obtener el usuario autenticado
@@ -19,59 +27,51 @@ class UserController extends Controller
             return response()->json(['error' => 'Usuario no autenticado'], 401);
         }
 
-        // Datos del usuario autenticado
+        // Preparar los datos del usuario autenticado
         $userData = [
             'year' => $user->year,
             'full_name' => $user->full_name,
             'email' => $user->email,
         ];
 
+        // Verificar si se encontraron los datos del usuario
         if (!$userData) {
             return response()->json(['message' => 'Usuario no encontrado'], 404);
         }
 
+        // Devolver los datos del usuario en formato JSON
         return response()->json(['user' => $userData]);
     }
 
+    /**
+     * Actualiza el perfil del usuario autenticado.
+     *
+     * @param  Request  $request
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function updateProfile(Request $request)
     {
         try {
             // Obtener el usuario autenticado
             $user = JWTAuth::user();
 
+            // Verificar si el usuario está autenticado
             if (!$user) {
                 return response()->json(['error' => 'Usuario no autenticado'], 401);
             }
 
-            $error = [];
-
-            // Verificar si el nombre completo es igual
-            if ($request->input('full_name') === $user->full_name) {
-                $error['full_name'] = 'El nombre completo no ha presentado cambios.';
-            }
-
-            // Verificar si el correo electrónico es igual
-            if ($request->input('email') === $user->email) {
-                $error['email'] = 'El correo electrónico no ha presentado cambios.';
-            }
-
-            // Verificar si el año es igual (usando == para comparación)
-            if ($request->input('year') == $user->year) {
-                $error['year'] = 'El año de nacimiento no ha presentado cambios.';
-            }
-
-            // Si hay errores, devolver los mensajes de error
-            if (!empty($error)) {
-                return response()->json(['error' => $error], 400);
-            }
-
-            // Validación de datos para la actualización de la contraseña
+            // Validar los datos para la actualización del perfil
             $messages = validationMessages();
             $this->validate($request, [
                 'full_name' => 'required|string|min:10|max:150',
-                'email' => 'required|string|regex:/^[^@]+@[^@.]+.[^@]+$/|ends_with:ucn.cl,alumnos.ucn.cl,disc.ucn.cl,ce.ucn.cl|unique:users,email',
+                'email' => 'required|string|regex:/^[^@]+@[^@.]+.[^@]+$/|ends_with:ucn.cl,alumnos.ucn.cl,disc.ucn.cl,ce.ucn.cl|unique:users,email,' . $user->id,
                 'year' => 'required|numeric|min:1900|max:' . (int)date('Y'),
             ], $messages);
+
+            // Almacenar los valores existentes antes de la actualización
+            $oldFullName = $user->full_name;
+            $oldEmail = $user->email;
+            $oldYear = $user->year;
 
             // Actualizar la información del usuario
             $user->full_name = $request->input('full_name');
@@ -79,33 +79,51 @@ class UserController extends Controller
             $user->year = $request->input('year');
             $user->save();
 
-            // Respuesta JSON con información de la actualización exitosa de la contraseña
-            return response()->json([
-                'success' => 'Información actualizada exitosamente',
-            ], 200);
+            // Verificar si se realizaron cambios antes de enviar la respuesta
+            $changesMade = (
+                $oldFullName !== $user->full_name ||
+                $oldEmail !== $user->email ||
+                $oldYear !== $user->year
+            );
 
+            // Generar la respuesta basada en los cambios realizados
+            if ($changesMade) {
+                return response()->json([
+                    'success' => 'Información actualizada exitosamente',
+                ], 200);
+            } else {
+                return response()->json([
+                    'success' => 'No se realizaron cambios en la información',
+                ], 200);
+            }
         } catch (\Illuminate\Validation\ValidationException $e) {
-            // Error de validación
+            // Manejar errores de validación
             $errors = $e->validator->errors()->getMessages();
             return response()->json(['errors' => $errors], 422);
         } catch (\Exception $e) {
-            // Excepción general
+            // Manejar excepciones generales
             return response()->json(['error' => $e->getMessage()], 500);
         }
     }
 
-
+    /**
+     * Actualiza la contraseña del usuario autenticado.
+     *
+     * @param  Request  $request
+     * @return \Illuminate\Http\JsonResponse
+     */
     public function updatePassword(Request $request)
     {
         try {
             // Obtener el usuario autenticado
             $user = JWTAuth::user();
 
+            // Verificar si el usuario está autenticado
             if (!$user) {
                 return response()->json(['error' => 'Usuario no autenticado'], 401);
             }
 
-            // Validación de datos para la actualización de la contraseña
+            // Validar los datos para la actualización de la contraseña
             $messages = validationMessages();
             $this->validate($request, [
                 'password' => 'required|string|min:8',
@@ -129,11 +147,11 @@ class UserController extends Controller
             ], 200);
 
         } catch (\Illuminate\Validation\ValidationException $e) {
-            // Error de validación
+            // Manejar errores de validación
             $errors = $e->validator->errors()->getMessages();
             return response()->json(['errors' => $errors], 422);
         } catch (\Exception $e) {
-            // Excepción general
+            // Manejar excepciones generales
             return response()->json(['error' => $e->getMessage()], 500);
         }
     }
